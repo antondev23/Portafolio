a56a0105bb054480bb1cbcb2dade3872f4d5fa3b
antondev23 <antonio.ortega0323@gmail.com>
2026-09-23 22:54:35 +0200

    google tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ const IndexPage = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet" />
+      <link href="../components/google.html" rel="preload" as="html" />
       <Body  />
       
     </div>
